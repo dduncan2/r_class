6c1e05279a722e28866bcc3c2505499fedaa0d8b
jonathan <39495158+jonatronblah@users.noreply.github.com>
2018-11-04 21:38:56 -0500

    added comments

--- hems_difftime.R
+++ hems_difftime.R
@@ -1,3 +1,5 @@
+#this script is intended to provide, in hours, the average trip time for each agency: the total time from dispatch to hospital, the time from dispatch to first vitals taken and the time from first vitals to hospital. unfortunately some of my results don't make complete sense - a few of the agencites have negative values for timetovitals and/or the timetovitals + vitalstodestination values don't match the totaltime values. However, for more than half of the agencies, these match up perfectly. this leads me to believe some of the data might not have been entered correctly - but I may have just screwed something up too.
+
 HEMS_difftime <- HEMS_responses %>% 
    group_by(Agency) %>% 
    summarise(
